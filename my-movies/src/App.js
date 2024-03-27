3f855f5f5983c0cfdf1106b17b898dbff9538f58
BekCodingAddict <experte001@gmail.com>
2024-03-28 00:55:55 +0900

    My Movie React App : Building Navbar Movie Component has been Done!

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Header from "./Header";
+import Movie from "./Movie";
 
 function App() {
   return (
     <div>
-      <Header />
+      <Movie />
     </div>
   );
 }
--- my-movies/src/Header.js
+++ my-movies/src/Header.js
@@ -1,33 +1,98 @@
 function Header() {
   return (
-    <header id="header">
-      <ul>
-        <li>
-          <a href="" className="active">
-            Home
+    <div className="header">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light" href="#">
+            My MOVIE <span className="logo">🎞️ </span>
           </a>
-        </li>
-        <li>
-          <a href="" className="active">
-            Movies
-          </a>
-        </li>
-        <li>
-          <a href="" className="active">
-            Cartoons
-          </a>
-        </li>
-        <li>
-          <a href="" className="active">
-            Recently Added
-          </a>
-        </li>
-      </ul>
-
-      <a href="" className="logo">
-        MY MOVIE
-      </a>
-    </header>
+          <button
+            className="navbar-toggler nav-toggle-btn"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon text-light"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active text-light"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Movies
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Cartoons
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle text-light"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li> */}
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-light text-light search-btn"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
