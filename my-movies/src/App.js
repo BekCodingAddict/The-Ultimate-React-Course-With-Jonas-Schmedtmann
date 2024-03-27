70d46943a7a2f64840310200e1051fa4d073fb4a
BekCodingAddict <experte001@gmail.com>
2024-03-27 17:37:04 +0900

    My Movie React App Initial Setting Up has been done!

---
+++ my-movies/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-movies/src/Header.js
@@ -0,0 +1,34 @@
+function Header() {
+  return (
+    <header id="header">
+      <ul>
+        <li>
+          <a href="" className="active">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="" className="active">
+            Movies
+          </a>
+        </li>
+        <li>
+          <a href="" className="active">
+            Cartoons
+          </a>
+        </li>
+        <li>
+          <a href="" className="active">
+            Recently Added
+          </a>
+        </li>
+      </ul>
+
+      <a href="" className="logo">
+        MY MOVIE
+      </a>
+    </header>
+  );
+}
+
+export default Header;
