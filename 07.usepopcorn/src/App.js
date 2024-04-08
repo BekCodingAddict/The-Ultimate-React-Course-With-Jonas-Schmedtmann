72a3e33ed259df2c87f1c9c4974cd57236e21484
BekCodingAddict <experte001@gmail.com>
2024-04-08 14:49:23 +0900

    Practice to Clean Up Data Fetching

--- 07.usepopcorn/src/App.js
+++ 07.usepopcorn/src/App.js
@@ -95,12 +95,14 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -108,8 +110,9 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not Found!");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") setError(err.message);
         } finally {
           setIsLoading(false);
         }
@@ -120,6 +123,9 @@ export default function App() {
         return;
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
