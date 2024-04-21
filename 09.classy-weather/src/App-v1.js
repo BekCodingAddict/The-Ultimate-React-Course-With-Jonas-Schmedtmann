9fa7f448aa42fe3f04b2e047ce01426fe586d4fd
BekCodingAddict <experte001@gmail.com>
2024-04-21 20:27:11 +0900

    Child Prevent Comuminaciton

--- 09.classy-weather/src/App-v1.js
+++ 09.classy-weather/src/App-v1.js
@@ -81,18 +81,17 @@ class App extends React.Component {
       this.setState({ isLoading: false });
     }
   };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search from location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
         <button onClick={this.fetchWeater}>Get Weather</button>
         {this.state.isLoading && <p className="loader"> Loading</p>}
         {this.state.weather.weathercode && (
@@ -107,6 +106,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
