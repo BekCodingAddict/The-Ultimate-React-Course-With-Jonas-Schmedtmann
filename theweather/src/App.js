49389bfc79f2b2591ca165b24730b32e7b1392a0
BekCodingAddict <experte001@gmail.com>
2024-04-22 19:49:10 +0900

    The Weather React App

---
+++ theweather/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>The Weather</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
