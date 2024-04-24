8b676e38c535819b6f8d678ce3d4ad2f05cf0676
BekCodingAddict <experte001@gmail.com>
2024-04-24 19:48:48 +0900

    Fixed delete Weather List button position

--- theweather/src/CurrentLocations.js
+++ theweather/src/CurrentLocations.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { getWeatherCondition, getWeatherIcon } from "./WeatherPage";
-
 class Location extends React.Component {
   constructor(props) {
     super(props);
