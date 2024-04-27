148ed3dfce4d11bcdf08298e3258f614d7ab5907
BekCodingAddict <experte001@gmail.com>
2024-04-27 21:40:56 +0900

    Loading Questions from Fake API operation has been done!

--- 10.react-quiz/src/App.js
+++ 10.react-quiz/src/App.js
@@ -1,6 +1,37 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Error!");
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
