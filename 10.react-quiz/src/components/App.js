9b518143d121b396f8b61896dd6e89b7abd4df76
BekCodingAddict <experte001@gmail.com>
2024-04-28 16:50:09 +0900

    Restart Quiz operations has been done!

--- 10.react-quiz/src/components/App.js
+++ 10.react-quiz/src/components/App.js
@@ -55,6 +55,12 @@ const reducer = (state, action) => {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Error!");
   }
@@ -110,6 +116,7 @@ function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
--- 10.react-quiz/src/components/FinishScreen.js
+++ 10.react-quiz/src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
@@ -14,6 +14,12 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
         {Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
     </>
   );
 }
