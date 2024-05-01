7322a5f8d0e96e0a71a24b3da57a9f96809504fe
BekCodingAddict <experte001@gmail.com>
2024-05-01 18:57:07 +0900

    Implementing the Country List operation has been done!

--- 11.worldwise/src/App.jsx
+++ 11.worldwise/src/App.jsx
@@ -6,6 +6,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 const BASE_URL = "http://localhost:9000";
 
 function App() {
@@ -44,7 +45,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>From</p>} />
         </Route>
       </Routes>
---
+++ 11.worldwise/src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message={"Add your city by clicking on  city on the map"} />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <div className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </div>
+  );
+}
+
+export default CountryList;
