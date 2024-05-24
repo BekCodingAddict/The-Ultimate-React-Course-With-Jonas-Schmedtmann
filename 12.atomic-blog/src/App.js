f0c79de6442e5708b101c88fe1690f3f78066c26
BekCodingAddict <experte001@gmail.com>
2024-05-24 19:27:18 +0900

    Surprising Optimization Trick With children

--- 12.atomic-blog/src/App.js
+++ 12.atomic-blog/src/App.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { usePosts, PostProvider } from "./PostContext";
+import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -121,14 +122,17 @@ function FormAddPost() {
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
 
--- 12.atomic-blog/src/Test.js
+++ 12.atomic-blog/src/Test.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
+  const words = Array.from({ length: 100_00 }, () => "WORD");
   return (
     <ul>
       {words.map((word, i) => (
@@ -14,13 +14,32 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
