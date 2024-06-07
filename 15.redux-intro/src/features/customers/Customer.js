96a824b103e0a5048e58a44a5a7909e514feeed3
BekCodingAddict <experte001@gmail.com>
2024-06-07 23:04:31 +0900

    Connecting our Redux App with react

--- 15.redux-intro/src/features/customers/Customer.js
+++ 15.redux-intro/src/features/customers/Customer.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const customer = useSelector((store) => store.customer.fullName);
+
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
--- 15.redux-intro/src/index.js
+++ 15.redux-intro/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store";
+import { Provider } from "react-redux";
 
 store.dispatch({ type: "account/deposit", payload: 250 });
 console.log(store.getState());
@@ -10,6 +11,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
