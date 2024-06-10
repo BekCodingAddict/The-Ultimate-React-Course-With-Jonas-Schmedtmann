c677682bc43b32e76b4a50be5fa571658afbd1fd
BekCodingAddict <experte001@agmail.com>
2024-06-10 21:53:30 +0900

    Displaying a Loading Indicator task has been finished!

--- 16.fast-react-pizza/src/UI/AppLayout.jsx
+++ 16.fast-react-pizza/src/UI/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content</h1>
---
+++ 16.fast-react-pizza/src/UI/Loader.jsx
@@ -0,0 +1,5 @@
+function Loader() {
+  return <div className="loader"></div>;
+}
+
+export default Loader;
