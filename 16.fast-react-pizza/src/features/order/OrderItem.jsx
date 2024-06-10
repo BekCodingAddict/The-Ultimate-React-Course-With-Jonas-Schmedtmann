1197274d829b1663e084711b60c3d6c8840ffe1f
BekCodingAddict <experte001@agmail.com>
2024-06-10 18:10:45 +0900

    Setting Up a Professional File Structure has been done!

---
+++ 16.fast-react-pizza/src/features/order/OrderItem.jsx
@@ -0,0 +1,17 @@
+import { formatCurrency } from "../../utils/helpers";
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
