433459571a02de98d5baa32e5a3e46ad7bbe4280
BekCodingAddict <experte001@gmail.com>
2024-06-16 20:09:15 +0900

    Practice of Handling Error with Error Elements

--- 16.fast-react-pizza/src/App.jsx
+++ 16.fast-react-pizza/src/App.jsx
@@ -5,10 +5,12 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
+import Error from "./UI/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
--- 16.fast-react-pizza/src/UI/Error.jsx
+++ 16.fast-react-pizza/src/UI/Error.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
-
+  const error = useRouteError();
+  console.log(error);
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
