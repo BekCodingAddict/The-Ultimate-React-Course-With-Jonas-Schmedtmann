ce32bb9ce3c1b88b0250013bf3e6bfe6049f79ed
BekCodingAddict <experte001@gmail.com>
2024-06-23 20:27:30 +0900

    Styling Form Elements with Tailwind

--- 16.fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16.fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -67,12 +67,18 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
--- 16.fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16.fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -15,6 +15,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="placeholder:text-stone400 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
