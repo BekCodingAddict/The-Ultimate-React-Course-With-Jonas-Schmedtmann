7004454785af39f1230dad38e6af5fbc231f6221
BekCodingAddict <experte001@gmail.com>
2024-06-23 21:18:02 +0900

    Practice of reusing styles with @apply

--- 16.fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16.fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -53,13 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {fromErrors?.phone && <p>{fromErrors.phone}</p>}
         </div>
@@ -67,12 +67,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
