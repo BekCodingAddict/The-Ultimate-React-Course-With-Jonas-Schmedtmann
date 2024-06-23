3effc5335c1f19535b41227c9afb74e31cce0c8e
BekCodingAddict <experte001@gmail.com>
2024-06-23 17:03:58 +0900

    Setting Up Tailwind CSS has been completed!

--- 16.fast-react-pizza/src/UI/Home.jsx
+++ 16.fast-react-pizza/src/UI/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-center text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
