7f6f426cdaa83a74cbd9bc1b0ae5eb5bc37a9697
unknown <experte001@gmail.com>
2024-08-31 23:53:31 +0900

    Render Props-Demo Inital Commits

---
+++ render-pops-demo/src/App.js
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { faker } from "@faker-js/faker";
+import "./styles.css";
+
+const products = Array.from({ length: 20 }, () => {
+  return {
+    productName: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: faker.commerce.price(),
+  };
+});
+
+const companies = Array.from({ length: 15 }, () => {
+  return {
+    companyName: faker.company.name(),
+    phrase: faker.company.catchPhrase(),
+  };
+});
+
+function ProductItem({ product }) {
+  return (
+    <li className="product">
+      <p className="product-name">{product.productName}</p>
+      <p className="product-price">${product.price}</p>
+      <p className="product-description">{product.description}</p>
+    </li>
+  );
+}
+
+function CompanyItem({ company, defaultVisibility }) {
+  const [isVisible, setIsVisisble] = useState(defaultVisibility);
+
+  return (
+    <li
+      className="company"
+      onMouseEnter={() => setIsVisisble(true)}
+      onMouseLeave={() => setIsVisisble(false)}
+    >
+      <p className="company-name">{company.companyName}</p>
+      {isVisible && (
+        <p className="company-phrase">
+          <strong>About:</strong> {company.phrase}
+        </p>
+      )}
+    </li>
+  );
+}
+
+function List({ title, items }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const displayItems = isCollapsed ? items.slice(0, 3) : items;
+
+  function toggleOpen() {
+    setIsOpen((isOpen) => !isOpen);
+    setIsCollapsed(false);
+  }
+
+  return (
+    <div className="list-container">
+      <div className="heading">
+        <h2>{title}</h2>
+        <button onClick={toggleOpen}>
+          {isOpen ? <span>&or;</span> : <span>&and;</span>}
+        </button>
+      </div>
+      {isOpen && (
+        <ul className="list">
+          {displayItems.map((product) => (
+            <ProductItem key={product.productName} product={product} />
+          ))}
+        </ul>
+      )}
+
+      <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+        {isCollapsed ? `Show all ${items.length}` : "Show less"}
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Render Props Demo</h1>
+
+      <div className="col-2">
+        <List title="Products" items={products} />
+      </div>
+    </div>
+  );
+}
+
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
---
+++ render-pops-demo/src/HOC.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function withToggles(WrappedComponent) {
+  return function List(props) {
+    const [isOpen, setIsOpen] = useState(true);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const displayItems = isCollapsed ? props.items.slice(0, 3) : props.items;
+
+    function toggleOpen() {
+      setIsOpen((isOpen) => !isOpen);
+      setIsCollapsed(false);
+    }
+
+    return (
+      <div className="list-container">
+        <div className="heading">
+          <h2>{props.title}</h2>
+          <button onClick={toggleOpen}>
+            {isOpen ? <span>&or;</span> : <span>&and;</span>}
+          </button>
+        </div>
+        {isOpen && <WrappedComponent {...props} items={displayItems} />}
+
+        <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+          {isCollapsed ? `Show all ${props.items.length}` : "Show less"}
+        </button>
+      </div>
+    );
+  };
+}
