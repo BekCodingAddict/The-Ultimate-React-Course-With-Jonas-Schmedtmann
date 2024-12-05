3c56b07ece353e014eb3cf2a312c9d3796ca0363
unknown <experte001@gmail.com>
2024-12-06 01:42:43 +0900

    Creating Layout With a Custom_App

--- 22.Nextjs-Pages-Router/pages/_app.js
+++ 22.Nextjs-Pages-Router/pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import Navigation from "@/components/Navigation";
+import Header from "@/components/Header";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+    >
+      <Header />
+
+      <div className="flex-1 px-8 py-12 grid">
+        <main className="max-w-7xl mx-auto w-full">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </div>
+  );
+  // return <Component {...pageProps} />;
 }
