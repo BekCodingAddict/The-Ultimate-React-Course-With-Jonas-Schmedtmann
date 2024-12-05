762e7b9fd70a8fcd1a1936a538a8808525d0091f
unknown <experte001@gmail.com>
2024-12-06 01:15:46 +0900

    Inital Commit for Next.js Pages Router Practices

---
+++ 22.Nextjs-Pages-Router/pages/index.js
@@ -0,0 +1,16 @@
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return <h1>Hello Pages Router</h1>;
+}
