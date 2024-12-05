f067fddf687d6455bc68b6d6c03513cd33fb9dfe
unknown <experte001@gmail.com>
2024-12-06 02:11:18 +0900

    Defining pages title and favicons

--- 22.Nextjs-Pages-Router/pages/_app.js
+++ 22.Nextjs-Pages-Router/pages/_app.js
@@ -3,6 +3,7 @@ import Navigation from "@/components/Navigation";
 import Header from "@/components/Header";
 
 import { Josefin_Sans } from "next/font/google";
+import Head from "next/head";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -11,17 +12,23 @@ const josefin = Josefin_Sans({
 
 export default function App({ Component, pageProps }) {
   return (
-    <div
-      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
-    >
-      <Header />
+    <>
+      <Head>
+        <title>The Wild Oasis</title>
+        <link rel="icon" href="logo.png" />
+      </Head>
+      <div
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
 
-      <div className="flex-1 px-8 py-12 grid">
-        <main className="max-w-7xl mx-auto w-full">
-          <Component {...pageProps} />
-        </main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <Component {...pageProps} />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
   // return <Component {...pageProps} />;
 }
--- 22.Nextjs-Pages-Router/pages/cabins/[cabinId].js
+++ 22.Nextjs-Pages-Router/pages/cabins/[cabinId].js
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
   const router = useRouter();
-  return <div>Cabin #{router.query.cabinId}</div>;
+  return (
+    <>
+      <Head>
+        <title>Cabin {router.query.cabinId}</title>
+      </Head>
+      <div>Cabin #{router.query.cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
